6f973b76426f72e3d950587fee323ab092dce00d
Sachin Agarwal <sa9027269750@gmail.com>
2023-10-11 00:56:51 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+let imgBox = document.querySelector("#imgBox");
+let qrImg = document.querySelector("#qrImg");
+let qrText = document.querySelector("#qrText");
+
+function generateQR(){
+
+   if(qrText.value.length>0){
+
+    qrImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+
+    imgBox.classList.add("show-img");
+
+   }else{
+
+    qrText.classList.add("error");
+
+    imgBox.classList.remove("show-img");
+
+    setTimeout(()=>{
+        qrText.classList.remove("error");
+    }, 1000);
+
+   }
+}
+
+
+
+
+
